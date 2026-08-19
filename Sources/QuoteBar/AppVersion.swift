a5afac03345b54768db4ff539f28a8e54b4064b9
ansel <ansel@surfin.sg>
2026-08-19 18:20:38 +0800

    release: v0.1.9

--- Sources/QuoteBar/AppVersion.swift
+++ Sources/QuoteBar/AppVersion.swift
@@ -9,7 +9,7 @@ enum AppVersion {
            value != "1.0", !value.isEmpty {
             return value
         }
-        return "0.1.8"
+        return "0.1.9"
     }
 
     static var build: String {
