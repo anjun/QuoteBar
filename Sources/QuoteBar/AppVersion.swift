b41dd400208dc8c257ac06bdc4ecc647459276b6
ansel <ansel@surfin.sg>
2026-08-25 17:55:56 +0800

    release: v0.1.13

--- Sources/QuoteBar/AppVersion.swift
+++ Sources/QuoteBar/AppVersion.swift
@@ -9,7 +9,7 @@ enum AppVersion {
            value != "1.0", !value.isEmpty {
             return value
         }
-        return "0.1.12"
+        return "0.1.13"
     }
 
     static var build: String {
